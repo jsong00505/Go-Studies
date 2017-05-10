4da8cd355cbf4980726a6ba4e134e9d0903da0f3
jsong00505 <jsong00505@gmail.com>
2017-05-10 14:07:33 -0700

    chore: add comments

--- go-by-example/01-values/values.go
+++ go-by-example/01-values/values.go
@@ -1,3 +1,7 @@
+// Created by jsong on 10/05/2017. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import "fmt"
